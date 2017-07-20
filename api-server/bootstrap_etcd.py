b36b8bc94c4d81aca45160afe9b677859fd45963
Mihir <mihirp@dhcp-10-38-201-129.mhos.eng.vmware.com>
2017-07-19 22:08:09 -0700

    added bootstrap etcd script

---
+++ api-server/bootstrap_etcd.py
@@ -0,0 +1,43 @@
+import json
+import yaml
+import etcd
+import os
+from service_catalog_client import ServiceCatalogClient
+
+etcd_client = etcd.Client(host='etcd.kubelink.borathon.photon-infra.com', port=80)
+
+service_types = {"mysql" : "database",
+        "mariadb" : "database",
+        "wordpress" : "cms",
+        "drupal" : "cms"
+        }
+
+yaml_template_file = "../etcd/service-instance-template.yaml"
+with open(yaml_template_file, 'r') as stream:
+    yaml_dict = yaml.load(stream)
+
+#json_template = json.dumps(yaml_template)
+#json_dict = json.loads(json_tempate)
+
+standalone_prefix = "/standalone/"
+photo_prefix = "img/kubernetes/"
+
+
+catalog_client = ServiceCatalogClient("bora-catalog")
+output = catalog_client.get_catalog()
+
+for item in output["items"]:
+    service_id = item["externalID"]
+    service_name = item["metadata"]["name"].lower()
+    service_type = service_types[service_name]
+    key = standalone_prefix + service_type + "/" + service_id
+    d = {"name" : service_name}
+    d["type"] = service_type
+    d["photo"] = photo_prefix + service_name.lower().replace(" ", "")
+    config_dict = yaml_dict
+    config_dict["metadata"]["name"] = service_name
+    config_dict["spec"]["serviceClassName"] = service_name
+    d["config"] = json.dumps(config_dict)
+    etcd_client.write(key, d)
+
+etcd_client.write("/counter", 0)
