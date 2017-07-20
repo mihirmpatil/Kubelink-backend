1c8b015527932cca1ac19558a8d4f07f51dcf35e
Akhil Rao <akhilr@vmware.com>
2017-07-19 21:57:35 -0700

    Service Instance Creation Iteration 1

--- api-server/main.py
+++ api-server/main.py
@@ -10,7 +10,7 @@ CORS(app)
 def main():
     return "Hello World!!!"
 
-@app.route("/catalog/standalone")
+@app.route("/catalog/standalone", methods=["GET"])
 def get_catalog():
     client = ServiceCatalogClient("bora-catalog")
     output = client.get_catalog()
@@ -18,7 +18,7 @@ def get_catalog():
     classesOp = []
     for item in output["items"]:
         d = {"name": item["metadata"]["name"]}
-        d["imageSrc"]="img/kubernetes/k8s"
+        d["imageSrc"]="img/kubernetes/"+d["name"].lower().replace(" ","")
         d["id"]=item["externalID"]
         classesOp.append(d)
     resp = {}
@@ -26,8 +26,36 @@ def get_catalog():
     resp["message"]="Successful"
     resp["data"] = classesOp
     return jsonify(resp)
+
+@app.route("/catalog/standalone", methods=["POST"])
+def create_instance():
+    client = ServiceCatalogClient("bora-catalog",400)
+    servicename = request.args.get("name","")
+    resp = {}
+    if servicename == "" :
+        resp["status"] = "ERROR"
+        resp["message"]="Invalid Service Name."
+    else:
+        client.create_instance(servicename.lower().replace(" ",""))
+        resp["status"] = "OK"
+        resp["message"]="Successful"
+    return jsonify(resp)
         
-app.run(host="0.0.0.0",port=5000,debug=True)
+
+def get_success_response(data):
+    resp = {}
+    resp["status"] = "OK"
+    resp["message"]="Successful"
+    resp["data"] = data
+    return jsonify(resp)
+
+def get_error_response(message):
+    resp = {}
+    resp["status"] = "ERROR"
+    resp["message"]= message
+    return jsonify(resp)
+
+app.run(host="0.0.0.0",port=80,debug=True)
 
 
 
--- api-server/service_catalog_client.py
+++ api-server/service_catalog_client.py
@@ -3,23 +3,39 @@ import json
 
 class ServiceCatalogClient(object):
 
-    def __init__(self, contextName):
+    def __init__(self, contextName, counter=300):
         self.context = contextName
         self.decoder = json.JSONDecoder()
+        self.counter = counter 
         
-    def execute(self, cmd):
-        newcmd = cmd + " -o json --context "+self.context
+    def execute(self, cmd, read=False):
+        newcmd = cmd + " --context "+self.context
+        if read:
+            newcmd += " -o json"
         jsonText = subprocess.check_output(newcmd, shell=True)
-        return self.decoder.decode(jsonText)
+        if read:
+            return self.decoder.decode(jsonText)
+        else:
+            return None
 
     def get_catalog(self):
         command = "kubectl get serviceclasses"
-        return self.execute(command)
+        return self.execute(command, True)
 
+    def create_instance(self, service_class_name):
+        r = open("instances/"+service_class_name.lower()+".yaml").read()
+        r = r.replace("#name#",service_class_name.lower()+"--"+str(self.counter))
+        f = open("instances/"+service_class_name.lower()+"-"+str(self.counter)+".yaml","w")
+        f.write(r)
+        f.close()
+        command = "kubectl create -f " + "instances/"+service_class_name.lower()+"-"+str(self.counter)+".yaml"
+        self.counter+=1
+        self.execute(command)
+        return True
 
 
 def main():
-    print str(ServiceCatalogClient("bora-catalog").get_catalog())
+    print ServiceCatalogClient("bora-catalog").create_instance("mysql")
 
 #main()
 
